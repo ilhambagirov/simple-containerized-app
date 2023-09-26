c9601a705639caa83587cb4784c8b4a0221dbda5
ilhambagirov <ilhambb@code.edu.az>
2023-09-27 01:55:49 +0400

    init

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom'
+  },
+  server: {
+    port: 3001,
+    hmr: { host: '0.0.0.0' },
+    open: false
+  },
+})
